fbb13f1d648bfa9f5f0fd07e8fe6ee22dbda7699
ryotaro abe <orat8888+git@gmail.com>
2022-06-26 16:00:25 +0900

    [update]db connect

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,12 +1,16 @@
 //go:generate go run github.com/99designs/gqlgen generate
 package graph
 
-import "github.com/aberyotaro/gql_sample_api/graph/model"
+import (
+	"github.com/aberyotaro/gql_sample_api/graph/model"
+	"gorm.io/gorm"
+)
 
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
 type Resolver struct {
+	DB    *gorm.DB
 	todos []*model.Todo
 }
